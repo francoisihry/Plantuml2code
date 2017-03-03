1134254a62b56b9ac63f72e566004922932ae81a
Francois Ihry <francois.ihry@gmail.com>
2017-03-03 07:58:14 +0100

    deleted unusefull plant2uml func

--- main.py
+++ main.py
@@ -4,25 +4,19 @@ from textx.metamodel import metamodel_from_file
 from os.path import join, dirname
 
 DEBUG = False
+mm_plant = metamodel_from_file(join(dirname(__file__) , 'meta_model.tx'), debug=DEBUG)
+
 
 def debug(str):
     if DEBUG:
         print(str)
-
-def plant2model(plant_uml_path):
-    print("Building a model from {} ...".format(plant_uml_path))
-    this_folder = dirname(__file__)
-    mm_plant = metamodel_from_file(join(this_folder, 'meta_model.tx'), debug=False)
-
-    return mm_plant.model_from_file(plant_uml_path,debug=DEBUG)
-
 
 if __name__ == "__main__" :
     print(len(sys.argv))
     if len(sys.argv) != 2:
         print ("Please enter the plant_uml file path as parameter: \n"
                "./plant2py <plant_um file path>")
-
-    plant_uml_model = plant2model(sys.argv[1])
-
+    plant_uml_path = sys.argv[1]
+    print("Building a model from {} ...".format(plant_uml_path))
+    plant_uml_model = mm_plant.model_from_file(plant_uml_path, debug=DEBUG)
 
